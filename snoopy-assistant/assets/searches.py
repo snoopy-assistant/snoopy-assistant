26f2b9d96ac52d6dce090f029f6b3edf81b74f19
OBADA ALHAWJREH <obada7jaber7@gmail.com>
2021-07-02 18:21:08 +0300

    mic is work with one joke

--- snoopy-assistant/assets/searches.py
+++ snoopy-assistant/assets/searches.py
@@ -1,39 +1,100 @@
-
+import datetime
+import speech_recognition as sr
 from urllib import request
 import json
 import pyttsx3
-pyttsx3.speak('obada is great')
+
 
 #-----------------------------------------------------jokes
-url = 'http://official-joke-api.appspot.com/random_ten'
-r = request.urlopen(url)
-print(r.getcode())
-data = r.read()
-jsonData = json.loads(data)
-# print(jsonData)
-class Joke:
-    def __init__(self,setup,punchline):
-        self.setup = setup
-        self.punchline = punchline
+# url = 'http://official-joke-api.appspot.com/random_ten'
+# r = request.urlopen(url)
+# print(r.getcode())
+# data = r.read()
+# jsonData = json.loads(data)
+# # print(jsonData)
+# class Joke:
+#     def __init__(self,setup,punchline):
+#         self.setup = setup
+#         self.punchline = punchline
+#
+#     def __str__(self):
+#         return f'setup {self.setup} punchline {self.punchline}'
+#
+# jokes=[]
+# for j in jsonData:
+#     setup = j['setup']
+#     punchline = j['punchline']
+#     joke = Joke(setup, punchline)
+#     jokes.append(joke)
+#
+# print(f'Got {len(jokes)} jokes')
+#
+# for joke in jokes:
+#     print(joke)
+#
+#     pyttsx3.speak(joke.setup)
+#     pyttsx3.speak('joke punchline')
+#     pyttsx3.speak(joke.punchline)
+#___________________________________________________________________________________
+engine = pyttsx3.init('sapi5')
+voices=engine.getProperty('voices')
+engine.setProperty('voice','voices[0].id')
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
 
-    def __str__(self):
-        return f'setup {self.setup} punchline {self.punchline}'
+def wishMe():
+    hour=datetime.datetime.now().hour
+    if hour>=0 and hour<12:
+        speak("Hello,Good Morning")
+        print("Hello,Good Morning")
+    elif hour>=12 and hour<18:
+        speak("Hello,Good Afternoon")
+        print("Hello,Good Afternoon")
+    else:
+        speak("Hello,Good Evening")
+        print("Hello,Good Evening")
 
-jokes=[]
-for j in jsonData:
-    setup = j['setup']
-    punchline = j['punchline']
-    joke = Joke(setup, punchline)
-    jokes.append(joke)
+def takeCommand():
+    r=sr.Recognizer()
+    with sr.Microphone() as source:
+        print('listening...')
+        audio = r.listen(source)
 
-print(f'Got {len(jokes)} jokes')
+        try:
+            statement=r.recognize_google(audio,language='en-in')
+            print(f'user said:{statement}\n')
+        except Exception as e:
+            speak('Pardon me, please say that again')
+            return 'None'
+        return statement
+print('Loading your AI personal assistant G-One')
+speak("Loading your AI personal assistant G-One")
+wishMe()
 
-for joke in jokes:
-    print(joke)
 
-    pyttsx3.speak(joke.setup)
-    pyttsx3.speak('joke punchline')
-    pyttsx3.speak(joke.punchline)
+
+
+
+
+
+
+if __name__=='__main__':
+
+
+    while True:
+        speak("Tell me how can I help you now?")
+        statement = takeCommand().lower()
+        if statement==0:
+            continue
+
+        if "joke" in statement or "ok bye" in statement or "stop" in statement:
+            speak('python is easy')
+            print('python is easy')
+            break
+
+
+
 
 
 
